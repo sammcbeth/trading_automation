817cd0c378a51cae039806cb4f602abc7b724fb8
sammcbeth <sammcbeth@berkeley.edu>
2019-12-31 10:36:57 -0800

    momentum fixes

--- src/momentum.py
+++ src/momentum.py
@@ -10,6 +10,8 @@ from secrets import API_KEY, API_SECRET, APCA_API_BASE_URL
 
 OILD = 'OILD'
 OILU = 'OILU'
+buy = 'buy'
+sell = 'sell'
 
 
 class Momentum:
@@ -27,7 +29,7 @@ class Momentum:
         self.awaitMarketOpen()
         print("Market opened.")
         tracked_ticker = choice([OILU, OILD])
-        tracked_price = get_last_price(tracked_ticker)
+        tracked_price = self.get_last_price(tracked_ticker)
         tracked_data = {'tracked_ticker':tracked_ticker, 'tracked_price':tracked_price}
         # Rebalance the portfolio every minute, making necessary trades.
         while True:
@@ -54,7 +56,7 @@ class Momentum:
             else:
                 # Rebalance the portfolio.
                 tracked_data = self.algo(tracked_data)
-                time.sleep(10)
+                time.sleep(30)
 
     # Wait for market to open.
     def awaitMarketOpen(self):
@@ -70,77 +72,106 @@ class Momentum:
 
     def algo(self, tracked_data):
         positions = self.alpaca.list_positions()
+        if positions:
+            entry_price  = positions[0].avg_entry_price
+            avg_entry_price = ''
+            for price in entry_price:
+                avg_entry_price += price
+            avg_entry_price = float(avg_entry_price)
+            ticker = positions[0].symbol
+            symbol = ''
+            for letter in ticker:
+                symbol += letter
+            symbol = str(symbol)
+            quantity = positions[0].qty
+            qty = ''
+            for num in quantity:
+                qty += num
+            qty = int(qty)
+        
         if not positions:
             return self.look_for_buy(tracked_data)
         else:
-            return self.look_for_sell(positions[0]['avg_entry_price'], positions[0]['symbol'], positions[0]['qty'], tracked_data)
+            return self.look_for_sell(avg_entry_price, symbol, qty, tracked_data=tracked_data)
         
     def look_for_buy(self, tracked_data):
         """ Based upon the assumption that the account does not currently
         hold any position the function monitiors the tracked symbol and price
         for a buying opportunity.
         """
+        print('looking for buy')
+        print(tracked_data)
         ticker  = tracked_data['tracked_ticker']
         price  = tracked_data['tracked_price']
-        current_price = get_last_price(ticker)
+        current_price = self.get_last_price(ticker)
+        print(ticker, current_price)
         if current_price >= 1.005*price:
-            submit_order(symbol=ticker, side=buy, qty=50000//price)
-            tracked_data['tracked_price'] = get_last_price(ticker)
+            self.submit_order(symbol=ticker, side=buy, qty=50000//current_price)
+            tracked_data['tracked_price'] = self.get_last_price(ticker)
             return tracked_data
         elif current_price <= .995*price:
             if ticker == OILU:
-                submit_order(symbol=OILD, side=buy, qty=50000//price)
-                tracked_data['tracked_price'] = get_last_price(OILD)
+                current_price = self.get_last_price(OILD)
+                self.submit_order(symbol=OILD, side=buy, qty=50000//current_price)
+                tracked_data['tracked_price'] = self.get_last_price(OILD)
                 tracked_data['tracked_ticker'] = OILD
             else:
-                submit_order(symbol=OILU, side=buy, qty=50000//price)
-                tracked_data['tracked_price'] = get_last_price(OILU)
+                current_price = self.get_last_price(OILU)
+                self.submit_order(symbol=OILU, side=buy, qty=50000//current_price)
+                tracked_data['tracked_price'] = self.get_last_price(OILU)
                 tracked_data['tracked_ticker'] = OILU
             return tracked_data
         else:
             return tracked_data
 
 
-    def look_for_sell(entry_price, symbol, qty, tracked_data):
+    def look_for_sell(self, entry_price, symbol, qty, tracked_data):
         """ Based upon the account currently having positions the function monitors
         the current price of positions and looks for a sell opportunity.
         """
-        latest_price = get_last_price(symbol=symbol)
+        print('looking for sell')
+        print(symbol, entry_price)
+        latest_price = self.get_last_price(ticker=symbol)
+        print(symbol, latest_price)
         if latest_price >= entry_price*1.005:
-            submit_order(symbol=symbol, side=sell, qty=qty)
+            self.submit_order(symbol=symbol, side=sell, qty=qty)
             tracked_ticker = symbol
-            tracked_price = get_last_price(symbol)
+            tracked_price = self.get_last_price(symbol)
             return {'tracked_ticker':tracked_ticker, 'tracked_price':tracked_price}
         elif latest_price <= entry_price*.995:
-            submit_order(symbol=symbol, side=sell, qty=qty)
+            self.submit_order(symbol=symbol, side=sell, qty=qty)
             if symbol == OILD:
                 tracked_ticker = OILU
             else:
                 tracked_ticker = OILD
-            tracked_price = get_last_price(tracked_ticker)
+            tracked_price = self.get_last_price(tracked_ticker)
             return {'tracked_ticker':tracked_ticker, 'tracked_price':tracked_price}
         else:
             return tracked_data
         
 
 
-    def get_last_price(ticker):
+    def get_last_price(self, ticker):
         baseUrl = f'https://api.polygon.io/v1/last/stocks/{ticker.upper()}?apiKey={API_KEY}'
-        data = req.get(baseUrl).text
-        data = json.loads(data)
-        return data['last']['price']
+        try:
+            data = req.get(baseUrl).text
+            data = json.loads(data)
+            return data['last']['price']
+        except:
+            print('Failed to fetch price data. Sleeping and trying again')
+            time.sleep(30)
+            self.get_last_price(ticker)
+
 
 
     # Submit an order if quantity is above 0.
-    def submitOrder(self, qty, stock, side):
+    def submit_order(self, qty, symbol, side):
         if(qty > 0):
             try:
-                self.alpaca.submit_order(stock, qty, side, "market", "day")
-                print("Market order of | " + str(qty) + " " + stock + " " + side + " | completed.")
+                self.alpaca.submit_order(symbol, qty, side, "market", "day")
+                print("Market order of | " + str(qty) + " " + symbol + " " + side + " | completed.")
             except:
-                print("Order of | " + str(qty) + " " + stock + " " + side + " | did not go through.")
-            else:
-                print("Quantity is 0, order of | " + str(qty) + " " + stock + " " + side + " | not completed.")
+                print("Order of | " + str(qty) + " " + symbol + " " + side + " | did not go through.")
 
 
  
